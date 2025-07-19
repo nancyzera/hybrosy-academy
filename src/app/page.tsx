754614db642a9bf1e57d638a3833188aa1d58526
nancyzera <nancytetakwizera@gmail.com>
2025-07-19 15:07:23 +0200

    sealife landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+// import Image from "next/Image";
+
+export default function Home(){
+  return(
+    <><div className="pager">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </div>
+    <div className="money">
+
+    <div className="text">
+        <h1>Welcome to sealife website</h1>
+              <p>Explore the wonders of the ocean and its inhabitants.</p>
+           <button>Learn More</button>
+        <button>Subscribe</button>
+      </div>    
+      </div>
+      <div className="footer">
+
+        <p>&copy; 2025 Sealife. All rights reserved.</p>
+    
+      
+
+      </div></>
+  )
+}
